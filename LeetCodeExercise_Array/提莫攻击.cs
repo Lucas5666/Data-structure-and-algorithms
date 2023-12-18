d48e6bb1c41ff084b1510c7cc3520ea4cfc6d930
Lucas <18827394752@163.com>
2023-12-18 16:26:15 +0800

    12.18

---
+++ LeetCodeExercise_Array/提莫攻击.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LeetCodeExercise_Array
+{
+	public class 提莫攻击
+	{
+        //遍历 时间没越界就直接加 越界了就加差值 到最后一个直接加持续时间
+        public int FindPoisonedDuration(int[] timeSeries, int duration)
+        {
+            int re = 0;
+            int index = 0;
+            while(index <= timeSeries.Length - 1)
+            {
+                if (index == timeSeries.Length - 1)
+                    return re += duration;
+                if (timeSeries[index] + duration < timeSeries[index + 1])
+                {
+                    re += duration;
+                }
+                else
+                {
+                    re += (timeSeries[index + 1] - timeSeries[index]);
+                }
+                index++;
+            }
+
+            return re;
+        }
+    }
+}
+
